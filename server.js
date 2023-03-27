3afe09725293056488a87d15d9ff779f95b15bbe
SOUMOJIT-CHAKRABORTY <goutamghambir2018@gmail.com>
2023-03-28 03:45:35 +0530

    database is working fine

--- server.js
+++ server.js
@@ -1,6 +1,28 @@
-const app = require("express")();
+const express = require("express");
+const app = express();
 require("dotenv").config();
 const mongoose = require("mongoose");
+const cors = require("cors");
+const fs = require("fs");
+const Post = require("./models/Post");
+
+const data = JSON.parse(fs.readFileSync("./fakeEntries.json", "utf-8"));
+
+// console.log(data);
+
+const importData = async () => {
+  try {
+    await Post.create(data);
+    console.log("data successfully imported");
+    // to exit the process
+    process.exit();
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
+app.use(cors());
+app.use(express.json());
 
 mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true });
 const db = mongoose.connection;
@@ -9,5 +31,10 @@ db.once("open", () => console.log("Connected to MongoDB"));
 
 const laptopRouter = require("./Routers/routes");
 app.use("/laptops", laptopRouter);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+  //   importData();
+});
 
 app.listen(4000, () => console.log("Server is running on port 4000"));
